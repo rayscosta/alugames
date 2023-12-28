046c9ce3d0b32761a53c442d896c94d763d9c82d
rai <rhmg@protonmail.com>
2023-12-28 10:30:37 -0300

    Confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 function alterarStatus(id) {
-    // document.getElementById('game-1').setAttribute('disabled', true);
-    let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('.dashboard__item__img')
-    let button = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
-    // alert(nomeJogo.textContent);
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        button.textContent = 'Alugar';
-        button.classList.remove('dashboard__item__button--return');
+    let gameClicked = document.getElementById(`game-${id}`);
+    let image = gameClicked.querySelector('.dashboard__item__img')
+    let button = gameClicked.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicked.querySelector('.dashboard__item__name');
+    if (image.classList.contains('dashboard__item__img--rented')) {
+        image.classList.remove('dashboard__item__img--rented');
+        let result = confirm('Vocẽ deseja devolver este item?');
+        if (result) {
+            button.textContent = 'Alugar';
+            button.classList.remove('dashboard__item__button--return');
+        } else {
+            alert('Operação cancelada!')
+        }
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
+        image.classList.add('dashboard__item__img--rented');
         button.textContent = 'Devolver';
         button.classList.add('dashboard__item__button--return');
     }
